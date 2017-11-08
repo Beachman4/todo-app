71d45aacb25cc37ce2c8bfd1f0cb40630fcd7f1d
Aylon Armstrong <beachman19@gmail.com>
2017-11-08 05:57:45 -0600

    Fix this later

--- resources/views/emails/todo/item.blade.php
+++ resources/views/emails/todo/item.blade.php
@@ -2,13 +2,8 @@
 # Here are your todo items for today!
 
 @foreach($todoItems as $item)
-    @if($item->url)
-        @component('mail::button', ['url' => $item->url, 'color' => 'green'])
-            {{ $item->title }}
-        @endcomponent
-    @else
-        {{ $item->title }}
-    @endif
+    
+    {{ $item->title }}
 @endforeach
 
 Thanks,<br>
